8791a8d4c95b6ca4fb0fb7976e8beb4166579352
Senlis <gizmo.romick@gmail.com>
2018-03-05 13:26:37 -0900

    Refactored Creatures class into own file and added parameter security on deal damage function

--- main.py
+++ main.py
@@ -1,42 +1,4 @@
-class Creature:
-    '''
-    Class creature
-    Tracks characteristics and status of all living objects in game world
-    '''
-    def __init__(self):
-        '''
-        Initialization function
-        Summary: Right now, it only creates the health attribute
-        '''
-        self.health = 100
-
-    def deal_damage(self, damage):
-        '''
-        Deal damage function
-        Summary: reduces creature's health by param damage
-        Paramters: init damage -- indicates how much damage to deal
-        '''
-        self.health -= damage
-
-    def is_alive(self):
-        '''
-        Is alive function
-        Summary: Used to determine if the creature is above min health
-        Returns: returns false if health is below or equals to 0, otherwise true
-        '''
-        if self.health <= 0:
-            return False
-        else:
-            return True
-
-    def get_health(self):
-        '''
-        Get health function
-        Summary: Get function to return creatures health
-        Returns: creatures health as int
-        '''
-        return self.health
-
+from creature import Creature
 
 # initialize game variables
 game_over = False
@@ -50,11 +12,14 @@ while not game_over:
 
     choice = input("What do you want to do? ")
 
+    # deal damage to the enemy
     if choice == "attack":
         enemy.deal_damage(40)
-        if not enemy.is_alive():
-            break
-
+        if not enemy.is_alive():  # determine if the game is over
+            game_over = True
+            continue  # go back to beggining of loop
+        
+    # deal damage to the hero
     hero.deal_damage(20)
     if not hero.is_alive():
-        break
+        game_over = True
